4f1872b76dd27228d9a209e19358a269099c63c1
EllaMozes <ellamozes@gmail.com>
2023-05-03 23:43:08 +0300

    hardcoded image name

--- configs/main.tf
+++ configs/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "dream_instance" {
   instance_type                 = "t2.micro"
   key_name                      = "dream-key"
   ami                           = "ami-007855ac798b5175e"           # ubuntu
-  user_data_base64          = "IyEvYmluL2Jhc2gKYXB0LWdldCB1cGRhdGUKYXB0LWdldCBpbnN0YWxsIC15IGRvY2tlci5pbwpkb2NrZXIgcHVsbCAke3Zhci5kb2NrZXJfaW1hZ2V9CmRvY2tlciBydW4gLWQgLXAgNTAwMDo1MDAwICR7dmFyLmRvY2tlcl9pbWFnZX0="
+  user_data_base64          = "IyEvYmluL2Jhc2gKYXB0LWdldCB1cGRhdGUKYXB0LWdldCBpbnN0YWxsIC15IGRvY2tlci5pbwpkb2NrZXIgcHVsbCBlbW96ZXMvZHJlYW0KZG9ja2VyIHJ1biAtZCAtcCA1MDAwOjUwMDAgZW1vemVzL2RyZWFt"
 
   user_data_replace_on_change   = true
 }
